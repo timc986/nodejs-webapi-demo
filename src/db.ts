6329eff0fa105a7a7dedec40c766bb2f431127e1
timc986 <timothychan986@hotmail.com>
2021-02-25 16:45:50 +0000

    load all db connectionoptions from env apart from type

--- src/db.ts
+++ src/db.ts
@@ -7,19 +7,17 @@ export async function intializeDB(): Promise<void> {
     var connectionOptions: ConnectionOptions = {
         "type": "postgres",
         "host": process.env.TYPEORM_HOST,
-        "port": 5432,
+        "port": Number(process.env.TYPEORM_PORT),
         "username": process.env.TYPEORM_USERNAME,
         "password": process.env.TYPEORM_PASSWORD,
         "database": process.env.TYPEORM_DATABASE,
-        "synchronize": true,
-        "logging": false,
+        "synchronize": (process.env.TYPEORM_SYNCHRONIZE === 'true'),
+        "logging": (process.env.TYPEORM_LOGGING === 'true'),
         "migrationsTableName": process.env.TYPEORM_MIGRATIONSTABLENAME,
         "entities": [
             __dirname + '/entities/*.ts'
         ]
     };
-
-    console.log(connectionOptions);
 
     await createConnection(connectionOptions);
     logger.info('Database connected');
